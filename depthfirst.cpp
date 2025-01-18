135613330aa0d32e8b3d798904633ac14cbd8ae2
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-18 19:01:20 +0530

    made an iterative implementation of depthfirst using std::vector, useful for dense and large graphs.

--- depthfirst.cpp
+++ depthfirst.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
 
-
-void displaygraph(std::vector <std::vector <int>> &);
-void dfs(std::vector <std::vector <int>> &, int, std::vector <bool> &);
+void dfs(std::vector <std::vector <int>> &adjmat, int);
 int main()
 {
     int V = 4;
     std::vector <std::vector <int>> adjmat(V, std::vector <int>(V));
     adjmat[0][1] = 1;
-    adjmat[1][0] = 1;
-    adjmat[1][3] = 1;
     adjmat[0][2] = 1;
+    adjmat[1][0] = 1;
     adjmat[2][0] = 1;
-    adjmat[3][2] = 1;
-    adjmat[2][3] = 1;
-    adjmat[3][1] = 1;
-    displaygraph(adjmat);
-    std::vector <bool> visited(V, false);
-    dfs(adjmat, 0, visited);
-    return 0;
+    adjmat[0][3] = 1;
+    adjmat[3][0] = 1;
+    dfs(adjmat, 0);
 }
 
-void displaygraph(std::vector <std::vector <int>> &adjmat)
+void dfs(std::vector <std::vector <int>> &adjmat, int start)
 {
-    for (auto const &node:adjmat)
-    {
-        for (auto const &neighbour:node)
-        {
-            std::cout << neighbour << "\t";   
-        }
-        std::cout << "\n";
-    }
-}
-
-void dfs(std::vector <std::vector <int>> &adjmat, int start, std::vector <bool> &visited)
-{
+    std::vector <bool> visited(adjmat.size(), false);
+    std::stack <int> gstack;
+    gstack.push(start);
     visited[start] = true;
-    std::cout << "Visited node: " << start << std::endl;
-    for (int i = 0; i < adjmat[start].size(); i++)
+    while (!gstack.empty())
+    {
+        int node = gstack.top();
+        gstack.pop();
+        std::cout << "Visited node: " << node << std::endl;
+        for (int i = adjmat[node].size()-1; i >= 0; i--)
         {
-            if (!visited[i] && adjmat[start][i] == 1)
+            if (adjmat[node][i] == 1 && !visited[i])
             {
+                gstack.push(i);
                 visited[i] = true;
-                dfs(adjmat, i, visited);
             }
         }
+    }
 }
