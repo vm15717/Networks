887e84c148d4d8a3d849a786052ebb6d584283c6
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-28 10:52:05 +0000

    made functions to display the adjacency matrix and the sparse matrix

--- graph_csr.cpp
+++ graph_csr.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 void to_adjacencymatrix(std::vector <int> &, std::vector <int> &, std::vector <std::vector <int>> &);
-void display(std::vector <std::vector <int>> & );
+void display_adjacencymatrix(std::vector <std::vector <int>> & );
+void display_sparsematrix(std::vector <int> &, std::vector <int> &);
 
-int main()
+int main(void)
 {
     int num_nodes = 3;
     std::vector <int> row_indices;
@@ -15,23 +16,21 @@ int main()
     col_indices.push_back(2);
     std::vector <std::vector <int>> adjmat(num_nodes, std::vector <int>(num_nodes, 0));
     to_adjacencymatrix(row_indices, col_indices, adjmat);
-    display(adjmat);
+    display_adjacencymatrix(adjmat);
+    display_sparsematrix(row_indices, col_indices);
     return 0;
 }
 
 void to_adjacencymatrix(std::vector <int> &row_indices, std::vector <int> &col_indices, std::vector <std::vector <int>> &adjmat)
 {
-    for (int &row : row_indices)
+    for (int j = 0; j < row_indices.size(); j++)
     {
-        for (int &col: col_indices)
-        {
-            adjmat[row][col] = 1;
-            adjmat[col][row] = 1;
-        }
+        adjmat[row_indices[j]][col_indices[j]] = 1;
+        adjmat[col_indices[j]][row_indices[j]] = 1;
     }
 }
 
-void display(std::vector <std::vector <int>> &adjmat)
+void display_adjacencymatrix(std::vector <std::vector <int>> &adjmat)
 {
     for (const auto &node: adjmat)
     {
@@ -40,5 +39,13 @@ void display(std::vector <std::vector <int>> &adjmat)
             std::cout << neighbour << "\t";   
         }
         std::cout << "\n";
+    }
+}
+
+void display_sparsematrix(std::vector <int> &row_indices, std::vector <int> &col_indices)
+{
+    for (int j = 0; j < row_indices.size(); j++)
+    {
+        std::cout << "(" << row_indices[j] << ", " << col_indices[j] << ")" << ":  1" << std::endl;
     }
 }
