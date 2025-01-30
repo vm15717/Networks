af8a7c87c7c749bb378d1040796f553c67baeada
vm15717 <vm15717@my.bristol.ac.uk>
2025-01-30 19:40:34 +0000

    made code to convert a dense graph to sparse representation

--- graph_csr.cpp
+++ graph_csr.cpp
@@ -3,10 +3,11 @@
 void to_adjacencymatrix(std::vector <int> &, std::vector <int> &, std::vector <std::vector <int>> &);
 void display_adjacencymatrix(std::vector <std::vector <int>> & );
 void display_sparsematrix(std::vector <int> &, std::vector <int> &);
+void to_sparse(std::vector <std::vector <int>> &, std::vector <int> &, std::vector <int> &);
 
 int main(void)
 {
-    int num_nodes = 300;
+    int num_nodes = 3;
     std::vector <int> row_indices;
     std::vector <int> col_indices;
     // Define the graph
@@ -18,6 +19,11 @@ int main(void)
     to_adjacencymatrix(row_indices, col_indices, adjmat);
     display_adjacencymatrix(adjmat);
     display_sparsematrix(row_indices, col_indices);
+    std::vector <int> row_indices2;
+    std::vector <int> col_indices2;
+    std::cout << "Converting to sparse" << std::endl;
+    to_sparse(adjmat, row_indices2, col_indices2);
+    display_sparsematrix(row_indices2, col_indices2);
     return 0;
 }
 
@@ -47,5 +53,20 @@ void display_sparsematrix(std::vector <int> &row_indices, std::vector <int> &col
     for (int j = 0; j < row_indices.size(); j++)
     {
         std::cout << "(" << row_indices[j] << ", " << col_indices[j] << ")" << ":  1" << std::endl;
+    }
+}
+
+void to_sparse(std::vector <std::vector <int>> &adj_matrix, std::vector <int> &row_indices, std::vector <int> &col_indices)
+{
+    for (int i = 0; i < adj_matrix.size(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (adj_matrix[i][j])
+            {
+                row_indices.push_back(j);
+                col_indices.push_back(i);
+            }
+        }
     }
 }
