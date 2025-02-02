de2a8b0101f96a636c5c030190149c0b7affd9b3
vm15717 <vm15717@my.bristol.ac.uk>
2025-02-02 11:47:40 +0000

    made a dynamic implementation of a tree

--- cayley_tree.cpp
+++ cayley_tree.cpp
@@ -9,6 +9,8 @@ class TreeNode
 };
 
 void createcayley(TreeNode *, int, int);
+void display(TreeNode *, int depth = 0);
+void deleteTree(TreeNode *);
 
 int main()
 {
@@ -17,6 +19,8 @@ int main()
     TreeNode* root = new TreeNode(1);
     all_nodes++;
     createcayley(root, num_iters, num_nodes);
+    display(root);
+    deleteTree(root);
     return 0;
 }
 
@@ -33,4 +37,31 @@ void createcayley(TreeNode *root, int num_iters, int num_nodes)
             createcayley(child, num_iters - 1, num_nodes);
         }
     }
+}
+
+void display(TreeNode *root, int depth)
+{
+    if (!root) return;
+    else
+    {
+        for (int i = 0; i < depth; i++) std::cout << " ";
+        std::cout << root->node << std::endl;
+        for (auto child: root->children)
+        {
+            display(child, depth+1);
+        }
+    }
+}
+
+void deleteTree(TreeNode *root)
+{
+    if (!root) return;
+    else
+    {
+        for (auto child: root->children)
+        {
+            deleteTree(child);
+        }
+    }
+    delete root;
 }
